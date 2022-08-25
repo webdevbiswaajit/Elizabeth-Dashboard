ced6faa8f8b992e33bcdca652226c0679042b39a
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-08-25 19:34:03 +0600

    nav issue solve

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 const toggle = document.getElementById("toggle");
 const sidebar = document.getElementById("dashboard_left");
-const hide_link = document.getElementsByClassName("h_link");
-console.log(hide_link);
+
 toggle.onclick = function () {
   toggle.classList.toggle("active");
   sidebar.classList.toggle("active");
